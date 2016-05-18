0e90d3c95d8144d301cb33e3a5912a83367a2375
sou1t <asus527@ya.ru>
2016-05-18 22:10:56 +0300

    Initial Commit

---
+++ WillPower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WillPower
+//
+//  Created by Виталий Волков on 18.05.16.
+//  Copyright © 2016 Виталий Волков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
